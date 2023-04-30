f4d743cdfe72494d540a129382f4dae1132653fe
Micode360 <abayemiracle@gmail.com>
2023-04-30 18:22:38 +0100

    navbar created for admin

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -5,7 +5,7 @@ import Header from "../adminComponents/header"
 export default function AdminDarshboard() {
 
   return (
-    <main className="fixed top-0 bottom-0 left-0 right-0">
+    <main>
       <Head>
         <title>Admin | MyCover.ai</title>
       </Head>
